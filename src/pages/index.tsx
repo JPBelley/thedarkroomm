f8c5249eb8f578d38ecc339e1924c29fac86186c
jean-philippe f belley <jean-philippefbelley@jean-philippes-MacBook-Pro.local>
2024-12-11 23:23:48 -0500

    Carousel and letter animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,39 +2,61 @@ import * as React from "react"
 import type { HeadFC, PageProps } from "gatsby"
 // Import Swiper React components
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { EffectFade } from 'swiper/modules';
+import { EffectFade, Navigation } from 'swiper/modules';
+
+import ChevronLeft from "../assets/icons/chevron-left.svg";
+import ChevronRight from "../assets/icons/chevron-right.svg";
 
 // Import Swiper styles
 import 'swiper/css';
 import '../scss/index.scss';
 
-import HeroImg from '../images/01-1.jpg';
-import MountainImg from '../images/mountain.jpg';
+import ConconDune from '../images/01-1.jpg';
+import MountainImg from '../images/mountain-compressed.jpg';
+import Lighthouse from '../images/lighthouse.jpg';
+
+const heroImages = [
+  ConconDune, MountainImg, Lighthouse
+]
 
 const IndexPage: React.FC<PageProps> = () => {
   const [hover, setHover] = React.useState(false);
+  const [swiperReady, setSwiperReady] = React.useState(false);
 
   return (
     <main>
-      <div className={`hero ${hover && ' hovered'}`}>
-        <button 
-          className="hero-button" 
-          type="button"
+      <div className={`hero ${hover && ' hovered'} ${swiperReady && ' swiper-ready'}`}>
+        <h1 
+          className="hero-button"
           onMouseEnter={() => setHover(true)}
           onMouseLeave={() => setHover(false)}
         >
-          JPBelley
-        </button>
+          <span>J</span>
+          <span>P</span>
+          <span>B</span>
+          <span>e</span>
+          <span>l</span>
+          <span>l</span>
+          <span>e</span>
+          <span>y</span>
+        </h1>
         <Swiper
           spaceBetween={50}
           slidesPerView={1}
-          modules={[EffectFade]} effect="fade"
+          effect="fade"
+          // loop={true}
+          navigation={true}
+          modules={[EffectFade, Navigation]}
+          onAfterInit={() => setSwiperReady(true)}
           // onSlideChange={() => console.log('slide change')}
           // onSwiper={(swiper) => console.log(swiper)}
         >
-          <SwiperSlide><img src={HeroImg} alt="Concon Dunes" /></SwiperSlide>
-          <SwiperSlide><img src={MountainImg} alt="Mountain Patagonia" /></SwiperSlide>
+          {heroImages.map((img) => (
+            <SwiperSlide style={{ height: '100dvh', width: '100wh' }}><img src={img} alt=""  loading="eager"/></SwiperSlide>
+          ))}
         </Swiper>
+        {/* <ChevronRight className="chevron-next" /> */}
+        {/* <ChevronLeft className="chevron-prev" /> */}
       </div>
     </main>
   )
