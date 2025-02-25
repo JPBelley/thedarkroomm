1de9ae82f616490554e4128f21b4492f4c10b5a5
jean-philippe f belley <jean-philippefbelley@jean-philippes-MacBook-Pro.local>
2025-02-24 22:01:33 -0500

    Clean LogRocket installation

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,9 +3,8 @@ import './src/styles/tailwind.css'
 
 
 export const onClientEntry = () => {
-    console.log(LogRocket);
     
-    // if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'production') {
         LogRocket.init('cdyunb/thedarkroommcom');
-    // }
+    }
 };
